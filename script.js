0a45634786bbb34d025228114f2af2ab71ce2381
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-06-28 00:44:24 +0100

    Updating the Hompage

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 // ##########  menu  ##########
 
-const hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
 const menuLink = document.querySelectorAll(".menu-link");
 
 hamburger.addEventListener("click", () =>{
   hamburger.classList.add("active");
   menu.classList.add("active");
+  // if (menu.classList.contains("active")) {
+  //   menu.classList.remove("active");
+  // }
 })
 
 menuLink.forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active");
   menu.classList.remove("active");
 }))
+
+// hamburger.classList.contains("active")
