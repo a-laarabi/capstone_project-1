901f15c0fa3f488665ab13291b43bfe692356e9a
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-06-28 14:33:08 +0100

    updating index.html & style.css

--- script.js
+++ script.js
@@ -18,3 +18,49 @@ menuLink.forEach((n) => n.addEventListener('click', () => {
 }));
 
 // hamburger.classList.contains('active')
+
+// ##########  equestrian  ##########
+
+const equestrian = [
+  {
+    name: 'abdelkebir ouaddar',
+    expert: '',
+    image: '',
+    age: '',
+  },
+
+  {
+    name: 'Kent Farrington',
+    expert: '',
+    image: '',
+    age: '',
+  },
+
+  {
+    name: 'McLain Ward',
+    expert: '',
+    image: '',
+    age: '',
+  },
+
+  {
+    name: 'Laura Kraut',
+    expert: '',
+    image: '',
+    age: '',
+  },
+
+  {
+    name: 'Margie Engle',
+    expert: '',
+    image: '',
+    age: '',
+  },
+
+  {
+    name: 'Adrienne Sternlicht',
+    expert: '',
+    image: '',
+    age: '',
+  },
+];
