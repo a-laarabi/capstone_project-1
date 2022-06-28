eb7ae1b1087ba1ce85ba009a4d22905f7f9ebd56
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-06-28 01:22:11 +0100

    fixing the linters

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // ##########  menu  ##########
 
-const hamburger = document.querySelector(".burger");
-const menu = document.querySelector(".menu");
-const menuLink = document.querySelectorAll(".menu-link");
+const hamburger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const menuLink = document.querySelectorAll('.menu-link');
 
-hamburger.addEventListener("click", () =>{
-  hamburger.classList.add("active");
-  menu.classList.add("active");
-  // if (menu.classList.contains("active")) {
-  //   menu.classList.remove("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.add('active');
+  menu.classList.add('active');
+  // if (menu.classList.contains('active')) {
+  //   menu.classList.remove('active');
   // }
-})
+});
 
-menuLink.forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  menu.classList.remove("active");
-}))
+menuLink.forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  menu.classList.remove('active');
+}));
 
-// hamburger.classList.contains("active")
+// hamburger.classList.contains('active')
