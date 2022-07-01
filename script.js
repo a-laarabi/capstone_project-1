0412c259bf3969c5b6f4c5a21067e743e645eb49
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-07-01 12:22:07 +0100

    index.html, about.html, style & script.js

--- script.js
+++ script.js
@@ -27,8 +27,27 @@ xButton.addEventListener('click', () => {
 
 const people = document.querySelector('.equestrian-ul');
 const more = document.querySelector('.more');
+const media = window.screen.width;
 
 const equestrian = [
+  {
+    name: 'abdelkebir ouaddar',
+    specialty: 'Show jumping',
+    image: 'img/ouddar.jpeg',
+    age: '59',
+    description: 'Moroccan equestrian. He competed at the 2016 Summer Olympics in the individual jumping event, in which he tied for 50th place',
+    position: 'left',
+  },
+
+  {
+    name: 'Charlotte Dujardin',
+    specialty: 'Dressage',
+    image: 'img/Charlotte_Dujardin.jpeg',
+    age: '36',
+    description: 'British dressage rider, equestrian and writer. A multiple World and Olympic champion, Dujardin has been described as the dominant dressage rider of her era.',
+    position: 'right',
+  },
+  
   {
     name: 'Kent Farrington',
     specialty: 'Show jumping',
@@ -66,9 +85,34 @@ const equestrian = [
   },
 ];
 
+if (media < 768) {
+  people.innerHTML = `
+        <li class="equestrian-li left">
+          <img src="img/ouddar.jpeg" alt="ouddar">
+          <ul>
+            <li><h4 class="equestrian-name">abdelkebir ouaddar</h4></li>
+            <li><p class="equestrian-job">Show jumping</p></li>
+            <hr class="hr-equestrian">
+            <li class="old">59 years old</li>
+            <li>Moroccan equestrian. He competed at the 2016 Summer Olympics in the individual jumping event, in which he tied for 50th place</li>
+          </ul>
+      </li>
+
+        <li class="equestrian-li right">
+          <img src="img/Charlotte_Dujardin.jpeg" alt="Kent Farrington">
+          <ul>
+            <li><h4 class="equestrian-name">Charlotte Dujardin</h4></li>
+            <li><p class="equestrian-job">Dressage</p></li>
+            <hr class="hr-equestrian">
+            <li class="old">36 years old</li>
+            <li>British dressage rider, equestrian and writer. A multiple World and Olympic champion, Dujardin has been described as the dominant dressage rider of her era.</li>
+          </ul>
+        </li>
+        `;
+
 more.addEventListener('click', () => {
   if (more.textContent === 'MORE ') {
-    for (let i = 0; i < equestrian.length; i += 1) {
+    for (let i = 2; i < equestrian.length; i += 1) {
       const newLi = document.createElement('li');
       newLi.classList.add('equestrian-li');
       newLi.classList.add(equestrian[i].position);
@@ -112,3 +156,24 @@ more.addEventListener('click', () => {
     more.innerHTML = 'MORE <i class="fa fa-angle-down"></i>';
   }
 });
+
+} else if (media >= 768) {
+  for (let i = 0; i < equestrian.length; i += 1) {
+    const newLi = document.createElement('li');
+    newLi.classList.add('equestrian-li');
+    newLi.classList.add(equestrian[i].position);
+    newLi.innerHTML = `
+      <img src="${equestrian[i].image}" alt="${equestrian[i].name}">
+        <ul>
+          <li><h4 class="equestrian-name">${equestrian[i].name}</h4></li>
+          <li><p class="equestrian-job">${equestrian[i].specialty}</p></li>
+          <hr class="hr-equestrian">
+          <li class="old">${equestrian[i].age} years old</li>
+          <li>${equestrian[i].description}</li>
+        </ul>
+      `;
+    people.appendChild(newLi);
+}
+
+      more.parentElement.removeChild(more);
+}
